a68c2218dff07cdb97851df575bcb688bd345921
sushmitapereira <82155273+sushmitapereira@users.noreply.github.com>
2023-02-13 19:25:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function basicpage() {
+  window.location.href="basicpay.html";
+}
+function essentialpage() {
+  window.location.href="essentialpay.html";
+}
+function premiumpage() {
+  window.location.href="premiumpay.html";
+}
+function startpage() {
+  window.location.href="index.html";
+}
+function increment() {
+  document.getElementById('demoInput').stepUp();
+}
+function decrement() {
+  document.getElementById('demoInput').stepDown();
+}
